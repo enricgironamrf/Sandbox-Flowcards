f418e229d0cc5e0d8189a42694134d3621651c4c
Enric Girona <33842768+enricgironamrf@users.noreply.github.com>
2021-02-22 17:01:58 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var port = 3000;
+
+var app = express();
+
+// set public resoures folder
+app.use(express.static(__dirname + "/public"));
+
+// set your first route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+var server = http.createServer(app);
+server.listen(port, () => {
+  console.log("Server is starting = " + port);
+});
